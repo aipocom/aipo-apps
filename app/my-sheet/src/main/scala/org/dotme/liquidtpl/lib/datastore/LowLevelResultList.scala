ebe41db84f189b9d8e8c7f89dadf5148ea66c192
altakase <yusuke.takase.al@gmail.com>
2011-08-30 12:58:10 +0000

    Update Issue 46 add copyright
    git-svn-id: http://aipo-apps.googlecode.com/svn/trunk@196 0f44d557-f83f-8280-c7ab-769245d742fa

--- app/my-sheet/src/main/scala/org/dotme/liquidtpl/lib/datastore/LowLevelResultList.scala
+++ app/my-sheet/src/main/scala/org/dotme/liquidtpl/lib/datastore/LowLevelResultList.scala
@@ -1,3 +1,20 @@
+/*
+ * This file is part of official Aipo App.
+ * Copyright (C) 2011-2011 Aimluck,Inc.
+ * http://www.aipo.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.dotme.liquidtpl.lib.datastore
 import com.google.appengine.api.datastore.Entity
 import com.google.appengine.api.datastore.Query
